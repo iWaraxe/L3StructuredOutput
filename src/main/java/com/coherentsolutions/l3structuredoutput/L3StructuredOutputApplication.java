71fd217a4df56ec3d5f6b18090252902536487d4
iWaraxe <igor.waraxe@gmail.com>
2025-04-17 15:27:47 +0200

    Initial Commit

---
+++ src/main/java/com/coherentsolutions/l3structuredoutput/L3StructuredOutputApplication.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.l3structuredoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L3StructuredOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L3StructuredOutputApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coherentsolutions/l3structuredoutput/L3StructuredOutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.l3structuredoutput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L3StructuredOutputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
